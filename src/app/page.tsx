1df8296208a819caf84276c8183a39113089ab80
Alia <mail.kiansaziz@gmail.com>
2024-11-08 09:44:14 +0700

    crud

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,80 @@
-import Image from "next/image";
+"use client";
+import React, { useState , useRef} from "react";
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [items , setItems] = useState([]) as any;
+  const [model, setModel] = useState({
+    vegetable_name : '',
+    note: ''
+  })
+  const handleInputChange = (e : any) => {
+    const { name, value } = e.target;
+    setModel(prevModel => ({
+      ...prevModel,
+      [name]: value
+    }));
+  };
+  // const formRef = {
+  //   vegetable_name : useRef(null),
+  //   note: useRef(null)
+  // };
+  async function formAction(){
+    // const input = {
+    //   vegetable_name: formRef.vegetable_name.current,
+    //   note: formRef.note.current,
+    // };
+    console.log(model);
+    // console.log(formRef.vegetable_name);
+    // console.log(formRef.vegetable_name);
+    setItems([...items , model]);
+    // console.log(items);
+    // setItems[[...items, {name :''}]];
+  }
+  return <>
+      <form action={formAction}  className="max-w-sm mx-auto mt-5 border border-gray-300 bg-gray-100 p-5">
+        <div className="grid gap-6 mb-6">
+          <h1>Form</h1>
+          {/* <ul>
+            {items.map((item : any, index : number) => (
+              <li key={index}>
+                {item.vegetable_name}
+              </li>
+            ))}
+          </ul> */}
+          <div>
+            <label htmlFor="vegetable_name" className="text-sm font-medium block text-gray-900">Vegetable Name</label>
+            <input type="text" name="vegetable_name" onChange={handleInputChange} value={model.vegetable_name} className="bg-gray-50 border border-gray-300 text-gray-900 rounded-sm w-full p-1" />
+          </div>
+          <div>
+            <label htmlFor="" className="text-sm font-medium block text-gray-900">Note</label>
+            <textarea name="note" value={model.note} onChange={handleInputChange}  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-sm w-full p-1"></textarea>
+          </div>
+          <div className="mt-1 text-right">
+            <button type="submit" className="focus:outline-none text-white bg-blue-400 hover:bg-blue-300 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-1.5 me-2 mb-2 dark:focus:ring-blue-100">Save</button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </form>
+      
+      <div>
+        <table className="m-5 w-full mt-5 text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead>
+            <tr className="bg-white border">
+              <th>No</th>
+              <th>Vegetable Name</th>
+              <th>Note</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item : any, index : number) => (
+              <tr className="bg-white border ">
+                <td>{index +1}</td>
+                <td>{item.vegetable_name}</td>
+                <td>{item.note}</td>
+              </tr>  
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
 }
